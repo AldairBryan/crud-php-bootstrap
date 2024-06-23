cac0ae68bde66dc179fe2cf3b54eac3975b38bd2
AldairBryan <asalcedoch@unsa.edu.pe>
2024-06-23 18:19:46 -0500

    Table added

--- app/movies/index.php
+++ app/movies/index.php
@@ -11,6 +11,27 @@
 <body>
     <div class="container py-3">
         <h2 class="text-center">Movies</h2>
+
+        <div class="row justify-content-end">
+            <div class="col-auto">
+                <a href="#" class="btn btn-primary"><i class="fa-solid fa-circle-plus"></i> New Register</a>
+            </div>
+        </div>
+        <table class="table table-sm table-stripped table-hover mt-4">
+            <thead class="table-dark">
+                <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>Genre</th>
+                    <th>Poster</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
     </div>
     <script src="../../assets/js/bootstrap.bundle.min.js"></script>
 </body>
